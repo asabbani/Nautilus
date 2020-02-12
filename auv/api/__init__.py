881bd73ba3c2dd198a893c1a05de5b54680f9bd8
Kevin Medzorian <kevin.medzor@gmail.com>
2020-02-11 21:54:43 -0800

    tesT

--- auv/api/__init__.py
+++ auv/api/__init__.py
@@ -2,6 +2,6 @@ from .radio import Radio
 from .motor import Motor
 from .motor_controller import MotorController
 from .pid import PID
-from ms5837 import MS5837_30BA as PressureSensor  # Pressure Sensor
+from .ms5837 import MS5837_30BA as PressureSensor  # Pressure Sensor
 from IMU.BNO055 import BNO055 as IMU  # Intertial Measurement Unit
 from .radio import Radio
