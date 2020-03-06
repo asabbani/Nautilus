bcf92aee49ff1efa4c541ea59a767cfbdd26a192
Yonder - Software <yonderdeep@wireless-169-228-83-243.ucsd.edu>
2020-03-05 21:03:37 -0800

    applied refactoring

--- auv/api/radio.py
+++ auv/api/radio.py
@@ -3,9 +3,12 @@ The radio class enables communication over wireless serial radios.
 """
 import serial
 
+TIMEOUT_DURATION = 2
+DEFAULT_BAUDRATE = 115200
+
 
 class Radio:
-    def __init__(self, serial_path, baudrate=115200):
+    def __init__(self, serial_path, baudrate=DEFAULT_BAUDRATE):
         """
         Initializes the radio object.
 
@@ -16,14 +19,14 @@ class Radio:
         self.ser = serial.Serial(serial_path,
                                  baudrate=baudrate, parity=serial.PARITY_NONE,
                                  stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,
-                                 timeout=2
+                                 timeout=TIMEOUT_DURATION
                                  )
 
     def write(self, message):
         """
         Sends provided message over serial connection.
 
-        message: A string message that is sent over seral connection.
+        message: A string message that is sent over serial connection.
         """
         self.ser.write(message)
 
@@ -33,11 +36,11 @@ class Radio:
         """
         return self.ser.readline()
 
-    def isOpen(self):
+    def is_open(self):
         """
         Returns a boolean if the serial connection is open.
         """
-        return self.ser.isOpen()
+        return self.ser.is_open()
 
     def flush(self):
         """
