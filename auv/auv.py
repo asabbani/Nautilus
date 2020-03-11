d87187f20579a7f80a04f8c2dc97cff6cdda79f1
Yonder - Software <yonderdeep@IMT-Lab-Mac-Pro.local>
2020-03-10 20:43:50 -0700

    syntax errors

--- auv/auv.py
+++ auv/auv.py
@@ -105,18 +105,19 @@ class AUV():
                     if len(cmd_array) >= MIN_FUNC_LEN and cmd_array[0] in self.methods:
                         if len(message) > 2 and "(" in message and ")" in message:
                             # Get possible function name
-                        possible_func_name = message[0:message.find("(")]
-                        if possible_func_name in self.methods:
-                            self.log("Received command from AUV: " + message)
-                            try:
-                                # Attempt to evaluate command. => Uses Vertical Pole '|' as delimiter
-                                eval(message)
+                            possible_func_name = message[0:message.find("(")]
+                            if possible_func_name in self.methods:
                                 self.log(
-                                    "Successfully evaluated command: " + message)
-                            except:
-                                # Send verification of command back to base station.
-                                self.log("Evaluation of command  " +
-                                         message + "  failed.")
+                                    "Received command from AUV: " + message)
+                                try:
+                                    # Attempt to evaluate command. => Uses Vertical Pole '|' as delimiter
+                                    eval(message)
+                                    self.log(
+                                        "Successfully evaluated command: " + message)
+                                except:
+                                    # Send verification of command back to base station.
+                                    self.log("Evaluation of command  " +
+                                             message + "  failed.")
 
                 elif self.before:
                     # Line read was EMPTY, but 'before' connection status was successful? Connection verification failed.
