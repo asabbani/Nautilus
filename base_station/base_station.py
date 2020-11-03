d6a451f89f506f34c5babedd3ffa1ef58955fbdb
eestabaya <estabayapapaya@gmail.com>
2020-11-02 18:26:25 -0800

    debug on xbox

--- base_station/base_station.py
+++ base_station/base_station.py
@@ -67,7 +67,7 @@ class BaseStation(threading.Thread):
 
         # Try to connect our Xbox 360 controller.
         try:
-            self.joy = Joystick()
+            #self.joy = Joystick() TODO
             if (joy.connected()):
                 self.log("Successfuly found Xbox 360 controller.")
                 self.nav_controller = NavController(self.joy)
