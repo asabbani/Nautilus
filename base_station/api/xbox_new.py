7eb492f4c914640e0eb4c77111f2e16f31b48a80
eestabaya <estabayapapaya@gmail.com>
2020-11-07 16:55:43 -0800

    added debug statements

--- base_station/api/xbox_new.py
+++ base_station/api/xbox_new.py
@@ -5,7 +5,9 @@ from xbox360controller import Xbox360Controller
 class Xbox(Xbox360Controller):
 
     def __init__(self):
+        print("attempting superclass")
         super.__init__(0, axis_threshold=0.2)
+        print("superclass did something")
 
     def leftX(self):
         return 0 #TODO
