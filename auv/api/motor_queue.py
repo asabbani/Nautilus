caf7e6b9655504c01e7206f5c52b914cc0351647
Alex Zhang <alexzhang1618@gmail.com>
2021-11-06 12:26:48 -0700

    code refactoring

--- auv/api/motor_queue.py
+++ auv/api/motor_queue.py
@@ -15,7 +15,7 @@ TURN_TARGET_TOLERANCE = 1
 FORWARD_CONTROL_TOLERANCE = 0.1  # Within 0.1 meters of target
 FORWARD_TARGET_TOLERANCE = 0.1
 KNOTS_TO_METERSPERSEC = 1852/3600
-DEBUG = False 
+DEBUG = False
 
 
 class MotorQueue(threading.Thread):
@@ -60,28 +60,31 @@ class MotorQueue(threading.Thread):
             try:
                 # get some data here
                 pass
-            except: 
+            except:
                 print("GPS not found")
                 return
 
         # modulo: a mod function that retains negatives (ex. -1 % 360 = -1)
         def modulo(x, y): return x % y if x > 0 else -1 * (abs(x) % y)
 
+        # turn_error: Calculates distance between target and heading angles
+        # modulo(target - heading, 360) -> absolute distance between both angles
+        # needed because imu only contains 0-360 degrees, need to account for
+        # negative angles and angles > 360
+        def turn_error(target, heading): return modulo(target - heading, 360)
+
         # Turning with PID, might need to tune PID values
         target = heading + y    # Target angle to turn to
         turn_pid = PID(self.mc, 0, TURN_CONTROL_TOLERANCE, TURN_TARGET_TOLERANCE, DEBUG)
         heading, _, _ = self.imu.read_euler()
 
-        # modulo(target - heading, 360) -> absolute distance between both angles
-        # needed because imu only contains 0-360 degrees, need to account for
-        # negative angles and angles > 360
-        turn_speed = turn_pid.pid(modulo(target - heading, 360))
+        turn_speed = turn_pid.pid(turn_error(target, heading))
 
         while turn_speed != 0:
             self.mc.update_motor_speeds([0, turn_speed, 0, 0])
             heading, _, _ = self.imu.read_euler()
-            turn_speed = turn_pid.pid(modulo(target - heading, 360))
-            print("turn speed: {0}, distance from target (degrees): {1}, heading: {2}".format(turn_speed, modulo(target - heading, 360), heading))
+            turn_speed = turn_pid.pid(turn_error(target, heading))
+            print("turn speed: {0}, distance from target (degrees): {1}, heading: {2}".format(turn_speed, turn_error(target, heading), heading))
             time.sleep(LOOP_SLEEP_DELAY)
 
         self.mc.zero_out_motors()
@@ -90,18 +93,18 @@ class MotorQueue(threading.Thread):
         # Move to place using PID
         forward_pos = 0
         forward_pid = PID(self.mc, x, FORWARD_CONTROL_TOLERANCE, FORWARD_TARGET_TOLERANCE, DEBUG)
-        turn_speed = turn_pid.pid(modulo(target - heading, 360))
+        turn_speed = turn_pid.pid(turn_error(target, heading))
         forward_speed = forward_pid.pid(forward_pos)
 
         while forward_speed != 0:
             # Reorient turning if imu says it is off from target
             heading, _, _ = self.imu.read_euler()
-            turn_speed = turn_pid.pid(modulo(target - heading, 360))
+            turn_speed = turn_pid.pid(turn_error(target, heading))
 
             # Figure out speed to use to move forward
-            #gps.update()
+            # gps.update()
             forward_speed = forward_pid.pid(forward_pos)
-            #forward_pos += gps.speed * KNOTS_TO_METERSPERSEC * LOOP_SLEEP_DELAY  
+            #forward_pos += gps.speed * KNOTS_TO_METERSPERSEC * LOOP_SLEEP_DELAY
             forward_pos += forward_speed * LOOP_SLEEP_DELAY  # TODO Update forward_speed to use GPS speed once GPS is done
 
             self.mc.update_motor_speeds([forward_speed, turn_speed, 0, 0])
