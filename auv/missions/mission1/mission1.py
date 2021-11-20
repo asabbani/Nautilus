91fc02b4e27edcf87a725cfad30c69038fbe1adb
clairma01 <38985660+clairma01@users.noreply.github.com>
2021-11-20 11:32:22 -0800

    mission1 logs

--- auv/missions/mission1/mission1.py
+++ auv/missions/mission1/mission1.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 MAX_DEPTH_METERS = 50.0
 NEAR_SURFACE_METERS = 0.5
 
@@ -15,18 +17,20 @@ class Mission1():
         # Assign our state to starting state.
         self.state = "START"
 
+        self.logpath = ""  # TODO: assign logpath
+
     def loop(self):
         """ Continuously running loop function, run by AUV main thread. """
+        depth = self.pressure_sensor.depth()
+        motor_speed_down = 0
         if self.state == "START":
             if self.motor_controller is not None and self.pressure_sensor is not None and self.IMU is not None:
                 # Begin our mission (start diving)
+                motor_speed_down = 50
                 self.motor_controller.update_motor_speeds([0, 0, 50, 50])
                 self.state = "DIVING"
 
         if self.state == "DIVING":
-            # Read Depth
-            depth = self.pressure_sensor.depth()
-
             # If we reached max depth
             if depth >= MAX_DEPTH_METERS:
                 # Turn off our motors
@@ -39,9 +43,9 @@ class Mission1():
                 self.hydrophone.start_recording()
 
         if self.state == "RISING":
-            # Read Depth
-            depth = self.pressure_sensor.depth()
-
             if depth <= NEAR_SURFACE_METERS:
                 self.hydrophone.end_recording()
                 self.state = "DONE"
+        log = open(self.logpath, "a")
+        log.write(datetime.utcnow(), "Depth:", depth, "Downward speed:", motor_speed_down, "Action:", self.state)
+        log.close()
