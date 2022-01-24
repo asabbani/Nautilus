d2dcefd51028deb6fd547cda7cfa675ae3c4228f
Alex Zhang <alexzhang1618@gmail.com>
2022-01-23 17:20:14 -0800

    add constants file for base station folder

--- base_station/constants.py
+++ base_station/constants.py
@@ -10,16 +10,3 @@ CONNECTION_TIMEOUT = 6
 # AUV Constants (these are also in auv.py)
 MAX_AUV_SPEED = 100
 MAX_TURN_SPEED = 50
-
-
-# Navigation Encoding
-NAV_ENCODE = 0b000000100000000000000000           # | with XSY (forward, angle sign, angle)
-XBOX_ENCODE = 0b111000000000000000000000          # | with XY (left/right, down/up xbox input)
-MISSION_ENCODE = 0b000000000000000000000000       # | with X   (mission)
-DIVE_ENCODE = 0b110000000000000000000000           # | with D   (depth)
-
-# Action Encodings
-HALT = 0b010
-CAL_DEPTH = 0b011
-ABORT = 0b100
-DL_DATA = 0b101
